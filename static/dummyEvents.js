f6bd7945908f6b746690442fb692abba3a6b0cb5
wajahatkhan98 <Wajahat@dev2serve.com>
2022-02-09 17:14:12 +0500

    MY WEB3 app

---
+++ static/dummyEvents.js
@@ -0,0 +1,27 @@
+export const dummyEvents = [
+    {
+      price: 17,
+      from: '0xWAJUU',
+      to: '0xMUGHEES',
+      date: '4 months ago',
+    },
+    {
+      price: 17,
+      from: '0xWAJUU',
+      to: '0xMUGHEES',
+      date: '4 months ago',
+    },
+    {
+      price: 17,
+      from: '0xWAJUU',
+      to: '0xMUGHEES',
+      date: '4 months ago',
+    },
+    {
+      price: 17,
+      from: '0xWAJUU',
+      to: '0xMUGHEES',
+      date: '4 months ago',
+    },
+  ]
+  
